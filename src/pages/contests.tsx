dec3450af04f9529f4a3b295e94f4390c79d269c
Ayush Verma <Ayush>
2025-10-23 11:14:00 +0530

    feat: add upcoming contest component in contest page

--- src/pages/contests.tsx
+++ src/pages/contests.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
+import { UpcomingContests } from "../common/components/UpcomingContests/UpcomingContests";
 
 function Contests() {
   return (
     <div>
-      
+      <div>
+        {/* Show Upcoming Contests */}
+            <UpcomingContests/>
 
+
+      </div>
+
+      <div>
+        {/* Show last events and thier questions */}
+        
+
+
+        </div>
     </div>
-  )
+  );
 }
 
-export default Contests
+export default Contests;
